32bbacc133c7986570c35187e02fdc40f483c27c
Fabio Murer <fabio.murer@studio.unibo.it>
2025-03-07 11:15:29 +0100

    optimizing write_buffer_guest

---
+++ guest_inspector.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "vm.h"
+
+void* vm_guest_to_host(struct vm* vm, uint64_t guest_addr);
+
+void read_string_host(struct vm* vm, uint64_t guest_string_addr, char* buf, size_t bufsiz);
+
+void write_string_guest(struct vm* vm, uint64_t guest_string_addr, char* buf, size_t bufsiz);
+
+void write_buffer_guest(struct vm* vm, uint64_t guest_buffer_addr, void* buf, size_t bufsiz);
--- vsyscall.c
+++ vsyscall.c
@@ -25,63 +25,7 @@
 #include "vsyscall.h"
 #include "utils.h"
 #include "vmm.h"
-
-
-void* vm_guest_to_host(struct vm* vm, u_int64_t guest_addr) {
-	struct kvm_translation transl_addr;
-	transl_addr.linear_address = guest_addr;
-
-	if (ioctl(vm->vcpufd, KVM_TRANSLATE, &transl_addr) < 0) {
-		panic("KVM_TRANSLATE");
-	}
-
-	if (transl_addr.valid == 0) {
-		fprintf(stderr, "KVM_TRANSLATE address: %p not valid\n", (void*)guest_addr);
-		exit(EXIT_FAILURE);
-	}
-
-	return (void*)((uint64_t)vm->memory + transl_addr.physical_address - GUEST_PHYS_ADDR);
-}
-
-void read_string_host(struct vm* vm, uint64_t guest_string_addr, char* buf, size_t bufsiz) {
-	size_t byte_read = 0;
-	char* host_addr = NULL;
-
-	memset(buf, 0, bufsiz);
-	do {
-		host_addr = vm_guest_to_host(vm, guest_string_addr + byte_read);
-		buf[byte_read] = *host_addr;
-		byte_read++;
-	} while (byte_read < bufsiz && *host_addr != '\0');
-}
-
-void write_string_guest(struct vm* vm, uint64_t guest_string_addr, char* buf, size_t bufsiz) {
-	size_t byte_written = 0;
-	char* host_addr = NULL;
-
-	do {
-		host_addr = vm_guest_to_host(vm, guest_string_addr + byte_written);
-		*host_addr = buf[byte_written];
-		byte_written++;
-	} while (byte_written < bufsiz && buf[byte_written] != '\0');
-
-	if (byte_written+1 < bufsiz) {
-		byte_written++;
-		host_addr = vm_guest_to_host(vm, guest_string_addr + byte_written);
-		*host_addr = '\0';
-	}
-}
-
-void write_buffer_guest(struct vm* vm, uint64_t guest_buffer_addr, void* buf, size_t bufsiz) {
-	size_t byte_written = 0;
-	char* host_addr = NULL;
-
-	do {
-		host_addr = vm_guest_to_host(vm, guest_buffer_addr + byte_written);
-		*host_addr = *(char*)(buf + byte_written);
-		byte_written++;
-	} while (byte_written < bufsiz);
-}
+#include "guest_inspector.h"
 
 bool is_syscall(struct vm* vm, struct kvm_regs* regs) {
 
