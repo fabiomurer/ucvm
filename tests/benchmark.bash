df94bd89425af95546e4c73e04a3a1ae78eb9e89
Fabio Murer <fabio.murer@studio.unibo.it>
2025-09-09 14:44:36 +0200

    better test script

--- tests/benchmark.bash
+++ tests/benchmark.bash
@@ -11,14 +11,17 @@ PROGRAM_PATH="$1"
 
 # Extract program name (last part of the path without any directory)
 PROGRAM_NAME=$(basename "$PROGRAM_PATH")
+COINTAINER_NAME="${PROGRAM_NAME#./}-oci"
 
 # Run the benchmark with hyperfine
 echo "Running benchmark for $PROGRAM_PATH"
 echo "Results will be saved to /tmp/$PROGRAM_NAME.md"
 echo
 
-hyperfine -i --export-markdown "/tmp/$PROGRAM_NAME.md" \
+hyperfine -w 3 --export-json "/tmp/$PROGRAM_NAME.json" \
   "$PROGRAM_PATH" \
+  "qemu-x86_64 $PROGRAM_PATH" \
   "../../release/ucvm -- $PROGRAM_PATH" \
-  "../../release/ucvm -p0 -- $PROGRAM_PATH" \
-  "qemu-x86_64 $PROGRAM_PATH"
+  "umvu $PROGRAM_PATH" \
+  "sudo docker run --runtime=runsc-systrap $COINTAINER_NAME" \
+  "sudo docker run --runtime=runsc-kvm $COINTAINER_NAME"
